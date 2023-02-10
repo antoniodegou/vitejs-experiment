a6100a40ceb765ac3bc00bad6bb06e8a6bc4bd9d
antonio de gou <thestockofantonio@gmail.com>
2023-02-10 10:28:28 +0000

    added dark light mode

--- main.js
+++ main.js
@@ -1,84 +1,44 @@
 import 'normalize.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/scss/bootstrap.scss'
-import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import { setupCounter } from './counter.js'
-// import * as drag from '/node_modules/dragjs/'
-// import * as ONECOLOR from '/node_modules/onecolor/'
-// import * as colorjoe from '/node_modules/colorjoe/'
-// import * as colorjoe from 'colorjoe'
-// import  '/node_modules/colorjoe/css/colorjoe.css'
+import './style.scss'
+
+/* __________
+-- DARK MODE --
+__________ */ 
 
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
 
-// setupCounter(document.querySelector('#counter'))
-
-
-// const joe = colorjoe.rgb(
-//   "my-color", 
-//   "#FFFFFF",
-//   [
-//     "alpha",
-//     ["fields",
-//   //  {space: 'RGB', limit: 255, fix: 0},
-//    {space: 'HSL'},
-//   //  'Hex'
-//   //  'hex', 'HSL', "RGB"
-//   ]
-// ]);
-
-// joe.on("change", color => console.log("Selecting " + color.css()));
-// joe.on("done", color => console.log("Selected " + color.css()));
-
-// var val = document.getElementById('rgbValue');
-
-// colorjoe.rgb('rgbPicker').on('change', function(c) {
-//     val.innerHTML = c.css();
-// });
-
-// requirejs(['../dist/colorjoe'], function(colorjoe) {
-//   var val = document.getElementById('rgbValue');
-
-//   colorjoe.rgb('rgbPicker').on('change', function(c) {
-//       val.innerHTML = c.css();
-//   });
-// });
+  const body = document.body;
+  const btn = document.getElementById("my_switch");
+  const preferenceQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  console.log(preferenceQuery)
+  const addDarkMode = () => {
+    body.classList.remove("light-mode");
+    body.classList.add("dark-mode");
+  };
+  
+  const addLightMode = () => {
+    body.classList.remove("dark-mode");
+    body.classList.add("light-mode");
+  };
+  
+  const toggleTheme = () =>
+    !body.classList.contains("dark-mode") ? addDarkMode() : addLightMode();
+  
+  const checkPreference = () =>
+    preferenceQuery.matches ? addDarkMode() : addLightMode();
+  
+  btn.addEventListener("change", toggleTheme);
+  preferenceQuery.addEventListener("change", checkPreference);
+  
+  // (() => checkPreference())();
 
 
-// requirejs(['/node_modules/colorjoe/dist/colorjoe.js'], function(colorjoe) {
-//   var val = document.getElementById('rgbValue');
 
-//   colorjoe.rgb('rgbPicker').on('change', function(c) {
-//       val.innerHTML = c.css();
-//   });
-// });
-
-// const colInput = document.getElementById("mypick")
-
-// colInput.addEventListener("input", function(e){
-//   // let col = e.target.attributes.value.nodeValue
-//   // console.log(col)
-//   console.log(colInput.value)
-//   document.getElementById("mypicklable").style.backgroundColor = `${colInput.value}`
-// });
+/* __________
+-- FORM VALIDATION --
+__________ */ 
 
 const hexInput = document.getElementById("hex_code")
 const form = document.getElementById("myForm")
